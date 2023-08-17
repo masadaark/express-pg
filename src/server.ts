bc674390ad50ca705b51211dbc04f9029d58f604
masadaark <masadaark@gmail.com>
2023-08-17 22:23:46 +0700

    route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-console.log('running')
+import express, { Request, Response } from "express"
+import { Query } from "express-serve-static-core"
+
+const app = express()
+
+app.get("/", (_req, res) => {
+    res.json({ result: "Http status is ok" })
+})
+
+export type Test = { message: string }
+
+export interface TypeRequestQuery<T extends Query> {
+    query: T
+}
+
+app.get("/test", (req: TypeRequestQuery<Test>, res: Response) => {
+    res.json(req.query)
+})
+
+
+app.listen(3000, () => console.log("Server is running..."))
