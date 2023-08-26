948e523f17901d5a1c4cf26e45b5db5c7315c985
apiwit.w@outlook.com <apiwit.w@outlook.com>
2023-08-26 03:33:00 +0000

    fix code

--- Seedang/src/db/migration/15_coverage.ts
+++ Seedang/src/db/migration/15_coverage.ts
@@ -2,8 +2,8 @@ import { Knex } from 'knex';
 
 export async function up(db: Knex): Promise<void> {
   await db.schema.withSchema('seedang').createTable('coverage', (table) => {
-    table.integer('id').primary();
-    table.string('coverage_detail').notNullable();
+    table.increments('id').primary();
+    table.json('coverage_detail').notNullable();
     table.timestamp('create_at', { useTz: false}).defaultTo(db.fn.now());
 
     console.log(`create table coverage success`)
--- Seedang/src/models/order.model.ts
+++ Seedang/src/models/order.model.ts
@@ -1,6 +1,16 @@
+import { InsurancePackage } from "./insurance.model"
+
 export interface OrderTable {
     id?: number
     user_id: number
+    destination_country: string
+    origin_country: string
+    start_date: Date
+    end_date: Date
+    package_name: string
+    package_price: number
+    income_rate_id: number
+    coverage_id: number
     transaction_id?: number
     created_at?: Date
 }
@@ -27,4 +37,11 @@ export interface OrderBalanceTable {
 export interface OrderStatusTable {
     id?:number,
     name:string
+}
+export interface CreateOrderModel {
+    originCountry?: string,
+    destinationCountry?: string,
+    startDate?: Date;
+    endDate?: Date;
+    package?: InsurancePackage
 }
